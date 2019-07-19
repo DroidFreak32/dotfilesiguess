07d2e56465f3f461c8d9897079dacf2d002caeb3
DroidFreak32 <rushabshah32@gmail.com>
2019-07-20 03:20:05 +0530

    add reqd packages

--- .config/i3/i3packages.sh
+++ .config/i3/i3packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yay -S nitrogen lxappearance brandr pacli compton playerctl mpd i3lock-color
+yay -S nitrogen lxappearance brandr pacli compton playerctl mpd i3lock-color rofi rofi-greenclip nerd-fonts-ubuntu-mono flameshot polkit-kde polkit-kde-agent clipit xfce4-power-manager pamac-tray nm-applet kcalc light ttf-ubuntu-font-family
